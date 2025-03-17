fa0b4f370aebae0631472cf85f417bf68a43b476
PeterZabila <petrozabila@gmail.com>
2025-03-17 21:07:59 +0200

    1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,6 +24,7 @@ useEffect(() => {
     <nav className={`p-[15px] fixed top-0 left-0 w-full ${background} border-b-[1px] border-slate-200 border-opacity-20 flex flex-row z-50`}>
        <NavLink to='/'><img src={logo} alt="logo" className='w-[80px] h-[30px] mr-[50px]' /></NavLink>
         <ul className='flex flex-row  text-slate-300'>
+            
             <li className='mr-[5px]'>
                 <NavLink to='/contact'>Contacts</NavLink>
                 
